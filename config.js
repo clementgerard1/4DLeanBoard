f08ef92c773034f8d0252d627a2528803ee82eb4
Clément Gérard <gerard.clement25@gmail.com>
2020-07-03 11:47:17 +0200

    minor

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ module.exports = {
 
 	"forgeRenderer" : true,
 
-	"socketServerIp" : "194.199.221.139", //"194.199.221.148",
+	"socketServerIp" : "194.199.221.148",//"194.199.221.139", 
 	"socketServerPort" : 3001,
 
-	"dataServerIp" : "194.199.221.139", //"194.199.221.148",
+	"dataServerIp" : "194.199.221.148",//"194.199.221.139", 
 	"dataServerPort" : 3003,
 
 	"autoDeskAccount" : "clement",
